63a6cff1321857b139cff73bd33b00fbee0a56d6
patb-github <165882561+patb-github@users.noreply.github.com>
2024-05-14 13:55:20 +0700

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,8 @@
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
+
+function introduce(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy " + hobby);
+}
